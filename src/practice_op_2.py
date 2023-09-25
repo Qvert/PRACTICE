984d89e87b78a2e0ccfd2c29df136a71759d334d
ONI-CHAN <glscharow@yandex.ru>
2023-09-25 15:00:15 +0000

    Add code

--- src/practice_op_2.py
+++ src/practice_op_2.py
@@ -2,15 +2,20 @@
 Practice 2 OP
 """
 
+
+def calculate_function(list_coord: list) -> None:
+    pass
+
+
 list_coordinates = []
 while True:
     coordinates = tuple(
         map(int, input("Please enter the coordinates of the point ").split())
     )
-    print(coordinates)
+
     if len(coordinates) != 3:
         print("I'm sorry, but you didn't enter the coordinates completely")
-        break
+        continue
 
     for elem in list_coordinates:
         for elem_inside in elem:
@@ -18,14 +23,23 @@ while True:
                 print(
                     "Sorry, but the coordinates of the point go beyond the specified parameter"
                 )
-                break
-
     list_coordinates.append(coordinates)
-    parameter = input("Do you want to continue entering coordinates? (Y/N)")
+    parameter = input("Select the following action:\n"
+                      "1. Exit the program: 1\n"
+                      "2. Continue entering coordinates: 2\n"
+                      "3. Go to the calculations: 3\n")
+    FLAG = False
 
-    if parameter == "N":
+    while True:
+        if parameter == '1':
+            FLAG = True
+            break
+        if parameter == '2':
+            break
+        if parameter == '3':
+            calculate_function(list_coordinates)
+
+    if FLAG:
         break
-    if parameter == "Y":
-        continue
 
-print(list_coordinates)
+
