39cab0393fed719d83d348cd73ee558a0064b57a
Шаров Глеб <glscharow@yandex.ru>
2023-10-23 16:30:11 +0000

    Finished practice_op_4

--- src/practice_op_4.py
+++ src/practice_op_4.py
@@ -1,27 +1,40 @@
+import os.path
+
 from slovar_alph import *
 
 
-def read_file(name_file: str) -> list[str]:
-    with open(name_file, encoding="utf8") as file:
-        file_read = file.readlines()
-    return file_read
-
-
 def write_file_enc(text: str, key: str) -> None:
-    with open("Encrypt.txt", encoding="utf8", mode="w") as file:
-        file.writelines(key)
+    """
+    Function write finished encrypt string
+    :param text: Encrypt text
+    :param key: Key
+    :return: None
+    """
+    with open("Encrypt string.txt", encoding="utf8", mode="w+") as file:
+        file.write(f"{key}\n")
         file.writelines(text)
 
 
 def write_file_decode(text: str, key: str) -> None:
-    with open("Decode.txt", encoding="utf8", mode="w") as file:
+    """
+    Function write decoding text
+    :param text: Decode text
+    :param key: Key
+    :return: None
+    """
+    with open("Decode string.txt", encoding="utf8", mode="w+") as file:
+        file.write(f"{key}\n")
         file.writelines(text)
-        file.writelines(key)
 
 
 def check_text_key(text: str, key: str) -> bool:
-    flag_rus = False
-    flag_eng = False
+    """
+    Check that the key and the source text are of the same alphabet
+    :param text: Source text
+    :param key: Key
+    :return: True or False
+    """
+    flag_rus, flag_eng = False, False
     for elem_t, elem_k in zip(text, key):
         if elem_t.upper() in rus_symbol or elem_k.upper() in rus_symbol:
             flag_rus = True
@@ -32,22 +45,24 @@ def check_text_key(text: str, key: str) -> bool:
     return True
 
 
-def encrypt_decode_word(text: str, key: str, parameter=None) -> str:
+def encrypt_decode_string(text: str, key: str, parameter=None) -> str:
     """
     :param parameter: Parameter for encryption or decryption
     :param text: Source text
     :param key: Encryption key
     :return: Decode text
     """
-    string_new_key = ""
-    string_new_text = ""
-    while True:
-        string_new_key += key
-        if len(text) < len(string_new_key):
-            break
-    string_new_key = string_new_key[: len(text)]
+    string_new_text = string_new_key_new = ""
+    string_new_key = key * len(text)
+    count_ = 0
+    for element in range(len(text)):
+        if text[element].upper() in rus_symbol or text[element].upper() in eng_symbol:
+            string_new_key_new += string_new_key[element - count_]
+        else:
+            count_ += 1
+            string_new_key_new += text[element]
 
-    for i, j in zip(text, string_new_key):
+    for i, j in zip(text, string_new_key_new):
         if i.upper() in rus_symbol:
             new_index_symbol = 0
             index_key_dict, index_text_dict = (
@@ -56,18 +71,17 @@ def encrypt_decode_word(text: str, key: str, parameter=None) -> str:
             )
             if parameter == "encrypt":
                 new_index_symbol = index_text_dict + index_key_dict
-            if parameter == "decode":
-                new_index_symbol = abs(index_text_dict - index_key_dict)
 
-            if new_index_symbol > 33 or new_index_symbol <= 0:
-                new_index_symbol = abs(new_index_symbol - 33)
+            if parameter == "decode":
+                new_index_symbol = index_text_dict - index_key_dict
+
+            if new_index_symbol > 32 or new_index_symbol < 0:
+                new_index_symbol = abs(abs(new_index_symbol) - 32)
             if i.isupper():
-                string_new_text += list(dict_rus_symbol_upper.keys())[
-                    new_index_symbol - 1
-                ]
+                string_new_text += list(dict_rus_symbol_upper.keys())[new_index_symbol]
             else:
                 string_new_text += list(dict_rus_symbol_upper.keys())[
-                    new_index_symbol - 1
+                    new_index_symbol
                 ].lower()
 
         if i.upper() in eng_symbol:
@@ -80,17 +94,15 @@ def encrypt_decode_word(text: str, key: str, parameter=None) -> str:
             if parameter == "encrypt":
                 new_index_symbol = index_text_dict + index_key_dict
             if parameter == "decode":
-                new_index_symbol = abs(index_text_dict - index_key_dict)
+                new_index_symbol = index_text_dict - index_key_dict
 
-            if new_index_symbol > 26 or new_index_symbol <= 0:
-                new_index_symbol = abs(new_index_symbol - 26)
+            if new_index_symbol > 25 or new_index_symbol < 0:
+                new_index_symbol = abs(abs(new_index_symbol) - 25)
             if i.isupper():
-                string_new_text += list(dict_eng_symbol_upper.keys())[
-                    new_index_symbol - 1
-                ]
+                string_new_text += list(dict_eng_symbol_upper.keys())[new_index_symbol]
             else:
                 string_new_text += list(dict_eng_symbol_upper.keys())[
-                    new_index_symbol - 1
+                    new_index_symbol
                 ].lower()
 
         if i.upper() not in eng_symbol and i.upper() not in rus_symbol:
@@ -100,8 +112,9 @@ def encrypt_decode_word(text: str, key: str, parameter=None) -> str:
 
 def main() -> None:
     """Main function"""
-    source_text, key = read_file("text.txt")
-    source_text_enc, key = read_file("Encrypt.txt")
+    f = open("text.txt", encoding="utf8", mode="r").readlines()
+    key, source_text = f[0].replace('\n', ''), f[1]
+
     while True:
         action = input(
             "Select an action.\n"
@@ -113,16 +126,25 @@ def main() -> None:
             case "1":
                 if check_text_key(source_text, key):
                     write_file_enc(
-                        encrypt_decode_word(source_text, key, "encrypt"), key
+                        encrypt_decode_string(source_text, key, "encrypt"), key
                     )
+                    print("Your string is encrypted\n")
                 else:
                     print(
                         "There are letters of different alphabet in the key or text\n"
                     )
             case "2":
-                write_file_decode(
-                    encrypt_decode_word(source_text_enc, key, "decode"), key
-                )
+                if os.path.isfile("Encrypt string.txt"):
+                    f = open("Encrypt string.txt", encoding="utf8", mode="r").readlines()
+                    key_decode, source_text_enc = f[0].replace('\n', ''), f[1]
+
+                    write_file_decode(
+                        encrypt_decode_string(source_text_enc, key_decode, "decode"),
+                        key_decode,
+                    )
+                    print("Your line has been decrypted\n")
+                else:
+                    print("I'm sorry, you haven't encrypted the string yet.")
             case "3":
                 break
 
--- src/slovar_alph.py
+++ src/slovar_alph.py
@@ -1,6 +1,6 @@
 eng_symbol, rus_symbol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
-count = 1
+count = 0
 dict_eng_symbol_upper, dict_rus_symbol_upper = dict(), dict()
 
 
@@ -8,7 +8,7 @@ for elem1, elem2 in zip(eng_symbol, eng_symbol.lower()):
     dict_eng_symbol_upper[elem1] = count
     count += 1
 
-count = 1
+count = 0
 for elem1, elem2 in zip(rus_symbol, rus_symbol.lower()):
     dict_rus_symbol_upper[elem1] = count
     count += 1
